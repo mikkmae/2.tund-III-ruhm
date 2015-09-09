09b78d8f1e6a39fea77a89c21cf25b5874a83c2d
Mikk Mäe <mikkmae@greeny.cs.tlu.ee>
2015-09-09 13:10:22 +0300

    password error msg

--- login.php
+++ login.php
@@ -3,6 +3,7 @@
 		//LOGIN.PHP
 		//echo $_POST["email"];
 		$email_error = "";
+		$pw_error = "";
 		//kontrollime et keegi vajutas input nuppu
 		if($_SERVER["REQUEST_METHOD"] == "POST") {
 			
@@ -13,6 +14,12 @@
 			if (empty($_POST["email"])) {
 				
 				$email_error = "See väli on kohustuslik";
+				
+				
+			}
+			if (empty($_POST["password"])) {
+				
+				$pw_error = "See väli on kohustuslik";
 				
 				
 			}
@@ -28,7 +35,7 @@
 	
 	<form action="login.php" method="post" >
 		<input type="email" name="email" placeholder="E-post"><?php echo $email_error;  ?> <br><br>
-		<input type="password" name="password" placeholder="Parool"> <br><br>
+		<input type="password" name="password" placeholder="Parool"><?php echo $pw_error;  ?> <br><br>
 		<input type="submit" value="Log in"> <br><br>
 	</form>
 	
