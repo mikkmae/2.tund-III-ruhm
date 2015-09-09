6c216576936df0118f1738073f8b39c22fd03bd8
Mikk Mäe <mikkmae@greeny.cs.tlu.ee>
2015-09-09 12:38:16 +0300

    login peidž ja teemad

---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+
+		//LOGIN.PHP
+		
+		
+
+
+?>
+<html>
+<head>
+	<title>Login peidž</title>
+</head>
+<body>
+	<h2>Login peidž</h2>
+	
+	<input type="email" name="email" placeholder="E-post"> <br><br>
+	<input type="password" name="password" placeholder="Parool"> <br><br>
+	<input type="button" name="nupp" value="Log in"> <br><br>
+	
+	
+	<h2>Create user</h2>
+	
+</body>
+</html>
