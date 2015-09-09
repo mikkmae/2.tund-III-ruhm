a9eb687bce42969670aa0199ca0372203d56728c
Mikk Mäe <mikkmae@greeny.cs.tlu.ee>
2015-09-09 11:33:24 +0300

    Big lines of code

--- helloworld.php
+++ helloworld.php
@@ -5,3 +5,21 @@
 	echo $first_name." ".$last_name;
 
 ?>    	    	    	  	 			 		
+<br>
+<?php
+
+	$age = 19;
+	
+	//testib loogikat jelu, juhul gei vanus on väiksem kui 18 siis kirjuta "alaealine"
+	//muuljuhul "täisealine"
+	
+	// if loogikatehe
+	
+	if ($age >=18) {
+		
+		echo "Oled täisealine.";
+	} else {
+		
+			echo "Oled alaealine.";
+	}
+?>
